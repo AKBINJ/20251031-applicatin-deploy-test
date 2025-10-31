c5a19c81f5b7d4366585c76aa06ca2cc77e9d9f9
AKBINJ <jyp10311@naver.com>
2025-10-31 14:52:02 +0900

    20251031

---
+++ spring-source-verification-demo/src/main/java/com/example/demo/service/DummyService.java
@@ -0,0 +1,11 @@
+package com.example.demo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DummyService {
+        public void checkNull() {
+        String str = "abs";
+        System.out.println(str.length());
+    }
+}
